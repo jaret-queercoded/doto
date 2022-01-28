f041f3c8672edc02517db142316f6f67dc6b1855
Jaret Screws <jaret@queercoded.io>
2022-01-28 16:36:45 -0500

    Start of a list to hold todo items

--- main.c
+++ main.c
@@ -7,6 +7,19 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
+struct todo_item {
+    char *msg;
+    int priority;
+};
+
+struct todo_node {
+    struct todo_item item;
+    struct todo_node *next;
+};
+
+struct todo_node *head = NULL;
+struct todo_node *current = NULL;
+
 const char *bad_dirs[] = {".", "..", ".git", "objs"};
 
 bool should_read_dir(char *path) {
@@ -32,6 +45,21 @@ void search_file(char *path) {
         line_count++;
         if(strstr(line, "TODO")) {
             printf("Line contains todo: %s", line);
+            struct todo_node *node = malloc(sizeof(struct todo_node));
+            node->item.msg = malloc(sizeof(char) * len);
+            strncpy(node->item.msg, line, len);
+            // TODO figure out a way to caluclate priority based on some index maybe number of Os
+            node->item.priority = 1;
+            node->next = NULL;
+            if(!current) {
+                current = node;
+            } else {
+                current->next = node;
+                current = current->next;
+            } 
+            if(!head) {
+                head = current;
+            }
         }
     }
     fclose(fp);
@@ -66,7 +94,16 @@ int read_dir(char *path) {
     return 0;
 }
 
+void print_list() {
+    struct todo_node *itr = head;
+    while(itr) {
+        printf("%s\n", itr->item.msg);
+        itr = itr->next;
+    }
+}
+
 int main(void) {
     read_dir(".");
+    print_list();
     return 0;
 }
